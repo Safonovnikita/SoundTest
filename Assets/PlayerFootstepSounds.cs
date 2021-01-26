9cb748531c68601c4d4fe9508e006aea1d4622ce
Nikita <Safonovnikita2@mail.ru>
2021-01-26 13:24:14 +0300

    Add Project

---
+++ Assets/PlayerFootstepSounds.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFootstepSounds : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    [SerializeField] string FootstepEvent = "";
+    [SerializeField] float footstepDistance = 1f;
+
+    Vector3 lastSoundPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+    	    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	float distance = Vector3.Distance(transform.position, lastSoundPosition);
+	if(distance > footstepDistance)
+	{
+            Debug.Log("Test");
+	    FMODUnity.RuntimeManager.PlayOneShot(FootstepEvent, transform.position);
+	    lastSoundPosition = transform.position;
+	}
+    }
+}
